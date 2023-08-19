fd909240103dda17b1c1be587e9ab951e937ee62
funame0 <huraltupe@gmail.com>
2023-08-20 02:41:53 +0900

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,18 +1,19 @@
 #!/bin/sh
 rm -r dist
 for v in v2 v3; do
+  echo starting build for $v...
   mkdir -p dist/$v
   cp -r src dist/$v/
   cd dist/$v
   mv src/$v.manifest.json src/manifest.json
   rm src/*.manifest.json
-  yarn run parcel build src/manifest.json --dist-dir build
+  yarn run parcel build src/manifest.json --dist-dir build --no-cache
   cp ../../LICENSE.txt src/
   cp ../../LICENSE.txt build/
   if [ $v == "v2" ]; then
-    zip -r check-x-rate-limits-for-firefox.zip *
+    zip -rq check-x-rate-limits-for-firefox.zip *
   elif [ $v == "v3" ]; then
-    zip -r check-x-rate-limits-for-chrome.zip *
+    zip -rq check-x-rate-limits-for-chrome.zip *
   fi
-  cd -
+  cd - > /dev/null
 done
