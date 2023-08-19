dad15280ad46a9556a4df66870f4ccaf469dae82
funame0 <huraltupe@gmail.com>
2023-08-20 01:58:52 +0900

    Generate zip on build

--- build.sh
+++ build.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 rm -r dist
-for v in v2 v3
-do
+for v in v2 v3; do
   mkdir -p dist/$v
   cp -r src dist/$v/
-  mv dist/$v/src/$v.manifest.json dist/$v/src/manifest.json
-  rm dist/$v/src/*.manifest.json
-  yarn run parcel build dist/$v/src/manifest.json --dist-dir dist/$v/build
-  cp LICENSE.txt dist/$v/src/
-  cp LICENSE.txt dist/$v/build/
+  cd dist/$v
+  mv src/$v.manifest.json src/manifest.json
+  rm src/*.manifest.json
+  yarn run parcel build src/manifest.json --dist-dir build
+  cp ../../LICENSE.txt src/
+  cp ../../LICENSE.txt build/
+  if [ $v == "v2" ]; then
+    zip -r check-x-rate-limits-for-firefox.zip *
+  elif [ $v == "v3" ]; then
+    zip -r check-x-rate-limits-for-chrome.zip *
+  fi
+  cd -
 done
